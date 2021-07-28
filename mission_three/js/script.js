e066cd23736a6649f04e5d4d5f10adcdffb7ce6b
ken0426 <kenkaitixyou0426@gmail.com>
2021-07-28 19:00:59 +0900

    いらない記述の削除

--- mission_three/js/script.js
+++ mission_three/js/script.js
@@ -7,8 +7,8 @@ window.addEventListener('load',()=>{
   let text = document.getElementsByClassName("text");
   let new_text = document.getElementsByClassName("new-text");
   let new_text_span = document.getElementsByClassName("new_text_span");
-  
-  
+
+
   let c = [];
   text[c.length].addEventListener('input',()=>{
     new_text[c.length].innerHTML = text[c.length].value;
@@ -25,117 +25,82 @@ window.addEventListener('load',()=>{
     let n = b.length;
 
 
+  let newElement_s = document.createElement("span");
+  let newTextNone = document.createTextNode(", ");
+  newElement_s.appendChild(newTextNone);
+  newElement_s.setAttribute("class", "new_text_span");
+  let title = document.getElementById("title");
+  title.appendChild(newElement_s);
 
-
-    //   let newElement_s = document.createElement("span");
-    // let newTextNone = document.createTextNode(", ");
-    // newElement_s.appendChild(newTextNone);
-    // newElement_s.setAttribute("class", "new_text_span");
-    // let title = document.getElementById("title");
-    // title.appendChild(newElement_s);
-    // let w = n - 1;
-    //   new_text_span[w].remove();
-
-
-    let newElement_s = document.createElement("span");
-    let newTextNone = document.createTextNode(", ");
-    newElement_s.appendChild(newTextNone);
-    newElement_s.setAttribute("class", "new_text_span");
-    let title = document.getElementById("title");
-    title.appendChild(newElement_s);
-
-let newElement = document.createElement("span");
+  let newElement = document.createElement("span");
   let newContent = document.createTextNode("new");
   newElement.appendChild(newContent);
   newElement.setAttribute("class", "new-text");
-  // let title = document.getElementById("title");
   title.appendChild(newElement);
 
-  
+  sum = app[n].after(clone_element);
+  op[d.length].style.display ="none";
+  b.push(sum);
+  d.push(0);
+  n = b.length;
+  minus[n].addEventListener('click',()=>{
+    if(text_lg[n].innerHTML > 0)
+    text_lg[n].innerHTML--&&text_sum.innerHTML--;;
+  });
+  plus[n].addEventListener('click',()=>{
+    text_lg[n].innerHTML++;
+    text_sum.innerHTML++;
+  });
 
-    // let newText = document.getElementsByClassName("new-text");
-      // let newTextNone = document.createTextNode(", ");
-      // newText[n].appendChild(newTextNone);
+  let sum_zero = document.getElementsByClassName("sum-zero");
+  sum_zero[n].addEventListener('click',()=>{
+    text_sum.innerHTML = text_sum.innerHTML - text_lg[n].innerHTML;
+    text_lg[n].innerHTML = 0;
+  });
 
-    
-    // clone_element = app[n].cloneNode(true);
-    sum = app[n].after(clone_element);
-    op[d.length].style.display ="none";
-    b.push(sum);
-    d.push(0);
-    n = b.length;
-    minus[n].addEventListener('click',()=>{
-      if(text_lg[n].innerHTML > 0)
-      text_lg[n].innerHTML--&&text_sum.innerHTML--;;
-    });
-    plus[n].addEventListener('click',()=>{
-      text_lg[n].innerHTML++;
-      text_sum.innerHTML++;
-    });
+  zero[n].addEventListener('click',()=>{
+    b.pop();
+    d.pop();
+    app[n].style.display ="none";
+    text_sum.innerHTML = text_sum.innerHTML - text_lg[n].innerHTML;
+    text_lg[n].innerHTML = 0;
+    new_text[n].remove();
+    let w = n - 1;
+    new_text_span[w].remove();
+  });
 
-    let sum_zero = document.getElementsByClassName("sum-zero");
-    sum_zero[n].addEventListener('click',()=>{
-      text_sum.innerHTML = text_sum.innerHTML - text_lg[n].innerHTML;
-      text_lg[n].innerHTML = 0;
-    });
-
-    // let u = new_text[n].remove();
-    //   let w = n - 1;
-    //   let y = new_text_span[w].remove();
-
-    zero[n].addEventListener('click',()=>{
-      b.pop();
-      d.pop();
-      app[n].style.display ="none";
-      text_sum.innerHTML = text_sum.innerHTML - text_lg[n].innerHTML;
-      text_lg[n].innerHTML = 0;
-      new_text[n].remove();
-      let w = n - 1;
-      new_text_span[w].remove();
-    });
-
-    text[n].addEventListener('input',()=>{
-      new_text[n].innerHTML = text[n].value;
-        // let newTextNone = document.createTextNode(", ");
-        // newText[n].appendChild(newTextNone);
-        
-    });
-    })
-
-
-    
-
-
-    let plus = document.getElementsByClassName("plus");
-    // let text_lg = document.getElementsByClassName("text-lg");
-    
-    plus[c.length].addEventListener('click',()=>{
-      text_lg[c.length].innerHTML++;
-      text_sum.innerHTML++;
-    });
-
-    let minus = document.getElementsByClassName("minus");
-    minus[c.length].addEventListener('click',()=>{
-      if(text_lg[c.length].innerHTML > 0)
-        text_lg[c.length].innerHTML--&&text_sum.innerHTML--;});
+  text[n].addEventListener('input',()=>{
+    new_text[n].innerHTML = text[n].value;
+  });
+  })
 
 
 
-    let sum_zero = document.getElementsByClassName("sum-zero");
-    sum_zero[c.length].addEventListener('click',()=>{
-      
-      text_sum.innerHTML = text_sum.innerHTML - text_lg[c.length].innerHTML;
-      text_lg[c.length].innerHTML = 0;
-    });
-
-    let zero = document.getElementsByClassName("zero");
-    zero[c.length].addEventListener('click',()=>{
-      app[c.length].style.display= "none";
-      text_sum.innerHTML = text_sum.innerHTML - text_lg[c.length].innerHTML;
-      new_text[c.length].innerHTML ="";
-    });
 
 
-    
+  let plus = document.getElementsByClassName("plus");
+  plus[c.length].addEventListener('click',()=>{
+    text_lg[c.length].innerHTML++;
+    text_sum.innerHTML++;
+  });
 
+  let minus = document.getElementsByClassName("minus");
+  minus[c.length].addEventListener('click',()=>{
+  if(text_lg[c.length].innerHTML > 0)
+    text_lg[c.length].innerHTML--&&text_sum.innerHTML--;});
+
+
+
+  let sum_zero = document.getElementsByClassName("sum-zero");
+  sum_zero[c.length].addEventListener('click',()=>{
+    text_sum.innerHTML = text_sum.innerHTML - text_lg[c.length].innerHTML;
+    text_lg[c.length].innerHTML = 0;
+  });
+
+  let zero = document.getElementsByClassName("zero");
+  zero[c.length].addEventListener('click',()=>{
+    app[c.length].style.display= "none";
+    text_sum.innerHTML = text_sum.innerHTML - text_lg[c.length].innerHTML;
+    new_text[c.length].innerHTML ="";
+  });
   })
